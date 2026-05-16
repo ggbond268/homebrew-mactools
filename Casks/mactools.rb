c260e4fb7e2f889e63efcb63352cb973b7f4ba8b
ggbond268 <me@ggbond.cc>
2026-05-16 19:33:22 +0800

    Update mactools to 0.15.0
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/mactools.rb
+++ Casks/mactools.rb
@@ -1,8 +1,8 @@
 cask "mactools" do
-  version "0.14.0"
-  sha256 "be8d1a0926cd415e50aa72b8110b71333eb78ae5888a4b4fd8301bb990f5dd96"
+  version "0.15.0"
+  sha256 "692e2496daabe969d29c38a725e92b875e3c77d7fe579e2c43662b99312b38e8"
 
-  url "https://github.com/ggbond268/MacTools/releases/download/v0.14.0/MacTools.dmg"
+  url "https://github.com/ggbond268/MacTools/releases/download/v0.15.0/MacTools.dmg"
   name "MacTools"
   desc "Menu bar toolbox"
   homepage "https://github.com/ggbond268/MacTools"
