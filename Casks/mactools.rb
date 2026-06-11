0dbfe93631e106c1ca6f1e024ae9231b7c4b6b77
ggbond268 <me@ggbond.cc>
2026-06-11 22:01:11 +0800

    Update mactools to 1.0.18
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/mactools.rb
+++ Casks/mactools.rb
@@ -1,8 +1,8 @@
 cask "mactools" do
-  version "1.0.17"
-  sha256 "304459e0280023b3ec671e5142e5aa9a5d52200b5079c38b69ea890a21faff67"
+  version "1.0.18"
+  sha256 "b8efb48f5877b09c0d1f2dcf82e887446ef5a8ac980fe9f9651fdfe96d565b35"
 
-  url "https://github.com/ggbond268/MacTools/releases/download/v1.0.17/MacTools.dmg"
+  url "https://github.com/ggbond268/MacTools/releases/download/v1.0.18/MacTools.dmg"
   name "MacTools"
   desc "Menu bar toolbox"
   homepage "https://github.com/ggbond268/MacTools"
