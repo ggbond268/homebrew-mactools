c2256e3164a99159edad5119148c8483faae9898
ggbond268 <me@ggbond.cc>
2026-06-23 11:45:34 +0800

    Update mactools to 1.0.23
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/mactools.rb
+++ Casks/mactools.rb
@@ -1,8 +1,8 @@
 cask "mactools" do
-  version "1.0.22"
-  sha256 "fd64ee8356f4906323a2ba651f48991df67aee24d54db405f2936c70d6e8d8a7"
+  version "1.0.23"
+  sha256 "5353ead491043c3eeba68b447f2c180e1673fb0e525acb1f816c211066732a72"
 
-  url "https://github.com/ggbond268/MacTools/releases/download/v1.0.22/MacTools.dmg"
+  url "https://github.com/ggbond268/MacTools/releases/download/v1.0.23/MacTools.dmg"
   name "MacTools"
   desc "Menu bar toolbox"
   homepage "https://github.com/ggbond268/MacTools"
